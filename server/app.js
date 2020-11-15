fca931313047d596165a74c2989bb68962391ffa
Mohsin Ali <mohammadmohsin@gmail.com>
2020-11-15 07:47:44 +0500

    Creating event with eventInput.

--- server/app.js
+++ server/app.js
@@ -5,16 +5,32 @@ const { buildSchema } = require('graphql');
 const app = express();
 
 app.use(bodyParser.json());
+const events = [];
 
 app.use('/graphql', graphqlHTTP({
   schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float
+      date: String!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
 
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -24,11 +40,18 @@ app.use('/graphql', graphqlHTTP({
   `),
   rootValue: {
     events: () => {
-      return ['Romantic Cooking', 'Sailing', 'All-night coding']
+      return events
     },
     createEvent: (args) => {
-      const eventName = args.name;
-      return eventName;
+      const event = {
+        _id: Math.random().toString(),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: new Date().toISOString()
+      }
+      events.push(event);
+      return event;
     }
   },
   graphiql: true
